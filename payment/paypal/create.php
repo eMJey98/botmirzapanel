87276652f8208bc58f5d9b269632ef8f196e3faf
eMJey98 <mj.ram76@gmail.com>
2025-07-17 17:49:49 +0200

    create.php

--- payment/paypal/create.php
+++ payment/paypal/create.php
@@ -1,1 +1,13 @@
+<?php
+require_once '../../config.php';
+require_once '../../functions.php';
+
+$order_id = generateUUID();
+$amount   = $_GET['amount'];      // به یورو
+$desc     = $_GET['desc'];
+
+$response = createPayPalOrder($amount, $order_id, $desc);
+
+header('Location: '.$response['approve']);
+exit;
 
